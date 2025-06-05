7404f9edf4999cd11f27ed5889845110dacbdc44
baongoc2502 <ngoclhb.ptit@gmail.com>
2025-06-05 20:33:01 +0700

    Add files via upload

---
+++ Day19+20_2025_05_06/Thực hiện code chỉnh sửa nút đăng nhập/script.js
@@ -0,0 +1,5 @@
+//phần đăng nhập tài khoản
+function showLoginForm() {
+      document.querySelector('.login-btn').style.display = 'none';
+      document.getElementById('loginForm').style.display = 'flex';
+    }
